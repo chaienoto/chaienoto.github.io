a15119da3b732c119b3de77a3da1f46831898415
cuongtx <cuongtx@natajsc.com.vn>
2020-07-22 14:04:48 +0700

    lab4 finish

--- lab4.js
+++ lab4.js
@@ -66,7 +66,7 @@ function clockWork(){
     if (clock != null){
         clearInterval(clock)
         clock = null
-        if (confirm("Đồng hồ đã dừng, bạn có muốn khởi động lại không \nBạn có thể tự khởi động lại bằng cách bâm vào nó một lần nữa")) startClock()
+        if (confirm("Đồng hồ đã dừng, bạn có muốn khởi động lại không \nBạn có thể tự khởi động lại bằng cách bấm vào nó một lần nữa")) startClock()
     } else startClock()
 }
 
@@ -74,6 +74,50 @@ function clockShow(){
     var time = new Date()
     document.getElementById("clock").innerHTML ="Bây giờ là : " + time.getHours() + " : " + time.getMinutes() + " : " + time.getSeconds()
 }
+
+//example3
+var student = {}
+student.getInfo = function(){
+    this.name = prompt("Nhập tên của bạn")
+    this.point = prompt("nhập số điểm")
+    if (isNaN(this.point) || this.point>10  ) alert("Điểm nhập không hợp lệ") 
+    else student.showInfo()
+}
+student.showInfo = function () {
+    if (this.point >=5) student.result = "Đậu"; else student.result = "Trượt mất tiu rồi"
+    document.getElementById("name").innerHTML = this.name;
+    document.getElementById("point").innerHTML = this.point;
+    document.getElementById("exam3Result").innerHTML = this.result;
+}
+
+//example4
+var myWindown = {}
+myWindown.openWindown = function(){
+    this.w = window.open("lab4_info.html", "Thông Tin Sinh Viên Thực hiện", "width = 400, height = 200, top = 50,  left = 60")
+    this.w.focus()
+}
+myWindown.closeWindown = function(){
+    this.w.close()
+}
+myWindown.autoMove = function(){
+    this.w.focus()
+    this.w.moveBy(20, 20)
+    
+}
+myWindown.print = function(){
+    this.w.focus()
+    this.w.print()
+    
+}
+myWindown.moveTo = function(){
+    x = prompt("Nhập tọa độ 1")
+    y = prompt("Nhập Tọa độ 2") 
+    this.w.focus()
+    if (isNaN(x) || isNaN(y)) alert("Tọa độ không hợp lệ")
+    else this.w.moveBy(x,y)
+   
+}
+
 
 
 
