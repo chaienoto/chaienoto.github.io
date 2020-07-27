b32f6d879a28f3f4e0d27f5034db79baeb985353
cuongtx <cuongtx@natajsc.com.vn>
2020-07-27 13:18:04 +0700

    fix exam1 when user input number

--- js/myJS.js
+++ js/myJS.js
@@ -3,13 +3,31 @@
 window.onload = function() {
     includeHTML()
     startClock()
+    checkformNotVaild()
 }
 var exam1 = {}
 exam1.timeCount = 0
+exam1.isNotCompelete = true
 exam1.showInput = function() {
     console.log(this.a + " " + this.b + " " + this.c)
 }
+function checkformNotVaild(){
+    
+        exam1.a = (document.getElementById("a").value)
+        exam1.c = (document.getElementById("c").value)
+        exam1.b = (document.getElementById("b").value)
+        console.log("vaild: " + exam1.isCompelete + " " + typeof exam1.isCompelete)
+        console.log("a: " + exam1.a + " " + typeof exam1.a)
+        console.log("b: " + exam1.b + " " + typeof exam1.b)
+        console.log("c: " + exam1.c + " " + typeof exam1.c)
+        if (exam1.a == null || exam1.b == null || exam1.c == null || exam1.a == "" || exam1.b == "" || exam1.c == ""){
+            exam1.isNotCompelete = true
+        } else  exam1.isNotCompelete = false
+           
+    
+     document.getElementById("startExample1Btn").disabled = exam1.isNotCompelete
 
+}
 function randomInput() {
     document.getElementById("a").value = parseInt(Math.random() * 10 + 1)
     document.getElementById("b").value = parseInt(Math.random() * 10 + 1)
@@ -19,7 +37,6 @@ function randomInput() {
 }
 
 function startExample1() {
-    document.getElementById("startExample1Btn").disabled = true
     exam1.a = (document.getElementById("a").value)
     exam1.c = (document.getElementById("c").value)
     exam1.b = (document.getElementById("b").value)
@@ -50,6 +67,7 @@ function startExample1() {
 }
 
 function showExample1Result() {
+    
     console.log("delta: " + exam1.delta + typeof exam1.delta)
     console.log("x1: " + exam1.x1 + " " + typeof exam1.x1)
     console.log("timeCount: " + exam1.timeCount + " " + typeof exam1.timeCount)
@@ -64,6 +82,9 @@ function showExample1Result() {
             hiddenView("block", "block")
             break;
     }
+    exam1.isNotCompelete = true
+    document.getElementById("myForm").reset()
+    document.getElementById("startExample1Btn").disabled = exam1.isNotCompelete
 }
 
 function hiddenView(s1, s2) {
@@ -73,6 +94,7 @@ function hiddenView(s1, s2) {
     document.getElementById("x1").style.display = s1;
     document.getElementById("x2").style.display = s2;
     document.getElementById("timeCount").innerHTML = "Số lần giải: " + exam1.timeCount;
+    document.getElementById("example1Input").innerHTML = exam1.a+"x<sup>2</sup> + "+exam1.b+"x + "+exam1.c+" = 0"
 
 }
 
