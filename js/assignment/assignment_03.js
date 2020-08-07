fe45af9e6aed10dff28f93493ce068f763a5544d
cuongtx <cuongtx@natajsc.com.vn>
2020-08-07 16:59:50 +0700

    add layout for asm 3

---
+++ js/assignment/assignment_03.js
@@ -0,0 +1,12 @@
+function reloadByPrice() {
+    // var x = document.getElementById("sortPrice").value;
+    // var itemPrice = document.getElementsByName("itemPrice");
+    
+    // for (let index = 0; index <= itemPrice.length -1; index++) {
+    //     if (parseInt(itemPrice[index].textContent) < parseInt(x)) 
+    //     itemPrice[index].parentElement.style.display = "block"
+    //     else itemPrice[index].parentElement.style.display = "none"
+    //     console.log("select: "+ parseInt(itemPrice[index].textContent) );
+    // }
+    
+}
