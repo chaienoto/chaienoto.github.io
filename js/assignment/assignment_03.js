9a81fd7d76fa605ce71e34e296f93f140dbc4463
cuongtx <cuongtx@natajsc.com.vn>
2020-08-07 17:14:10 +0700

    add more function in asm3

--- js/assignment/assignment_03.js
+++ js/assignment/assignment_03.js
@@ -1,12 +1,15 @@
 function reloadByPrice() {
-    // var x = document.getElementById("sortPrice").value;
-    // var itemPrice = document.getElementsByName("itemPrice");
+    var x = document.getElementById("sortPrice").value;
+    var itemPrice = document.getElementsByName("itemPrice");
     
-    // for (let index = 0; index <= itemPrice.length -1; index++) {
-    //     if (parseInt(itemPrice[index].textContent) < parseInt(x)) 
-    //     itemPrice[index].parentElement.style.display = "block"
-    //     else itemPrice[index].parentElement.style.display = "none"
-    //     console.log("select: "+ parseInt(itemPrice[index].textContent) );
-    // }
+    for (let index = 0; index <= itemPrice.length -1; index++) {
+        console.log("select: "+ index );
+        console.log("price: "+ parseInt(itemPrice[index].innerText) );
+        console.log("priceSelect: "+ parseInt(x) );
+        if (parseInt(itemPrice[index].innerText) <= parseInt(x)){
+            itemPrice[index].parentElement.style.display = ""
+        } 
+        else itemPrice[index].parentElement.style.display = "none"
+    }
     
 }
